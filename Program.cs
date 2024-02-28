4bf4240813d3d773b68696f9af380c2424b96a17
Aleksei da Silva <Alekseidasilva@hotmail.com>
2024-02-28 23:46:21 +0100

    Alterado as Configuracoes do App

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
+using CD_CD_on_Azure;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+var connectionString = app.Configuration.GetConnectionString("DefaultConnection");
+
+var secrets = new Secrets();
+app.Configuration.GetSection("secrets").Bind(secrets);
+
+
+app.MapGet("/", () =>new
+{
+    connectionString=connectionString,
+    secrets=secrets
+});
 
 app.Run();
---
+++ Secrets.cs
@@ -0,0 +1,8 @@
+﻿namespace CD_CD_on_Azure;
+
+public class Secrets
+{
+    public string JwtTokenSecret { get; set; }=String.Empty;
+    public string ApiKey { get; set; }=String.Empty;
+    public string JwtTokPrivateKeyenSecret { get; set; }=String.Empty;
+}
